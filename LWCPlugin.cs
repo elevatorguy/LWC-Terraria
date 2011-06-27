45bae18d86035410399cf8c861142d321223d955
Tyler <hidendra@griefcraft.com>
2011-06-27 02:26:23 -0300

    I forget why I even tried using VS2010!

--- LWCPlugin.cs
+++ LWCPlugin.cs
@@ -6,78 +6,78 @@ using Terraria_Server.Plugin;
 /* LWC-Terraria for TDSM (tdsm.org) */
 namespace LWC
 {
-    public class LWCPlugin : Plugin
-    {
+	public class LWCPlugin : Plugin
+	{
 
-        /* If LWC is enabled */
-        private bool enabled = false;
+		/* If LWC is enabled */
+		private bool enabled = false;
 
-        private void Log(string message)
-        {
-            Program.tConsole.WriteLine("[LWC] " + message);
-        }
+		private void Log(string message)
+		{
+			Program.tConsole.WriteLine("[LWC] " + message);
+		}
 
-        public override void Load()
-        {
-            Name = "LWC";
-            Description = "Chest protection mod";
-            Author = "Hidendra";
-            Version = "1.00-dev";
-            
-        }
+		public override void Load()
+		{
+			Name = "LWC";
+			Description = "Chest protection mod";
+			Author = "Hidendra";
+			Version = "1.00-dev";
+			
+		}
 
-        public override void Enable()
-        {
-            /* Register our events */
-            registerHook(Hooks.PLAYER_COMMAND);
-            registerHook(Hooks.PLAYER_CHEST);
-            registerHook(Hooks.TILE_BREAK);
+		public override void Enable()
+		{
+			/* Register our events */
+			registerHook(Hooks.PLAYER_COMMAND);
+			registerHook(Hooks.PLAYER_CHEST);
+			registerHook(Hooks.TILE_BREAK);
 
-            enabled = true;
-            Log("LWC has been enabled!");
-        }
+			enabled = true;
+			Log("LWC has been enabled!");
+		}
 
-        public override void Disable()
-        {
-            enabled = false;
-            Log("LWC has been disabled!");
-        }
+		public override void Disable()
+		{
+			enabled = false;
+			Log("LWC has been disabled!");
+		}
 
-        public override void onPlayerCommand(Terraria_Server.PlayerCommandEvent Event)
-        {
-            if(enabled == false)
-            {
-                return;
-            }
+		public override void onPlayerCommand(Terraria_Server.PlayerCommandEvent Event)
+		{
+			if(enabled == false)
+			{
+				return;
+			}
 
-            // split the args
-            string[] args = Event.getMessage().ToLower().Split(' ');
+			// split the args
+			string[] args = Event.getMessage().ToLower().Split(' ');
 
-            if(args.Length == 0)
-            {
-                return;
-            }
+			if(args.Length == 0)
+			{
+				return;
+			}
 
-            Player player = Event.getPlayer();
-            string command = args[0].Trim();
+			Player player = Event.getPlayer();
+			string command = args[0].Trim();
 
-            if(command.Equals("/cpublic"))
-            {
-                // player.sendMessage("Hello!");
-            }
-        }
+			if(command.Equals("/cpublic"))
+			{
+				// player.sendMessage("Hello!");
+			}
+		}
 
-        public override void onPlayerOpenChest(Terraria_Server.Events.ChestOpenEvent Event)
-        {
-            base.onPlayerOpenChest(Event);
-        }
+		public override void onPlayerOpenChest(Terraria_Server.Events.ChestOpenEvent Event)
+		{
+			base.onPlayerOpenChest(Event);
+		}
 
-        public override void onTileBreak(Terraria_Server.Events.TileBreakEvent Event)
-        {
+		public override void onTileBreak(Terraria_Server.Events.TileBreakEvent Event)
+		{
 			Tile tile = Event.getTile();
 			
-            base.onTileBreak(Event);
-        }
+			base.onTileBreak(Event);
+		}
 
-    }
+	}
 }
